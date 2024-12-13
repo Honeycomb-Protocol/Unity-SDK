4be3e24e819e826f937b2c5234853d7bd80d39b7
owais-star <iqbalowais35@gmail.com>
2024-12-13 23:38:17 +0500

    Update: sample test project

--- Samples~/Profiles/Scripts/example/data/state_manager/States.cs
+++ Samples~/Profiles/Scripts/example/data/state_manager/States.cs
@@ -9,7 +9,7 @@ public class AppState
     public Profile CurrentProfile { get; set; }
 
     // Honeynet Initialized Test Project for this Example you can create also your own project through following https://docs.honeycombprotocol.com/projects/
-    public string Project { get; set; } = "FNw49dTGPx111p1ttZrkf8nQ1GuPwVHp5dg2rP5q2QUG";
+    public string Project { get; set; } = "7fFLj9toRQkMVomCyECtbyDVfmMsXRDRgaKNQ22NDcZV";
 
 
     public UserInfoInput CreateProfileInfo = new()
